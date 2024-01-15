36dfddfb542cd94c02c0762e08bb005316840158
RNSModz <118450000+RNSModz@users.noreply.github.com>
2024-01-15 14:40:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 from os import environ
 
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
-ADMIN = int(environ.get("ADMIN", ""))          
-CAPTION = environ.get("CAPTION", "")
+API_ID = int(environ.get("API_ID", "22096977"))
+API_HASH = environ.get("API_HASH", "48e981f09b5790770f97ba13662e2c2e")
+BOT_TOKEN = environ.get("BOT_TOKEN", "6851228775:AAFVjRaLHPC0UhxQ8B2EvCLTTjotICcv-hE")
+ADMIN = int(environ.get("ADMIN", "5898278492"))          
+CAPTION = environ.get("CAPTION", "rnsmovie")
 
 # for thumbnail ( back end is MrMKN brain 😉)
 DOWNLOAD_LOCATION = "./DOWNLOADS"
